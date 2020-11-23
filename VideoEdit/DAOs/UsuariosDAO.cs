453ca4bf2699251b632a8d30c86e5488d7feadf7
SClasen95 <simonclasen1@outlook.com>
2020-11-23 18:39:21 -0300

    Correccion Login

--- VideoEdit/DAOs/UsuariosDAO.cs
+++ VideoEdit/DAOs/UsuariosDAO.cs
@@ -14,7 +14,7 @@ namespace VideoEdit.DAOs {
             var req = new RestRequest($"API/Login?nombre={usuario}&contraseña={contraseña}");            
             req.RequestFormat = DataFormat.Json;
             var respuesta = client.Get(req).Content;
-            return respuesta.Equals("true");
+            return respuesta.Equals("\"true\"");
         }
     }
 }
